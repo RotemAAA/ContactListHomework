5e09bd158ac09ebcdf1a15b9247a9e0e19eb5c0c
RotemAAA <rotem.guzman@gmail.com>
2020-02-19 19:46:34 +0200

    Initial commit

---
+++ app/src/main/java/com/guzman/rotem/contactlisthomework/Constants.java
@@ -0,0 +1,13 @@
+package com.guzman.rotem.contactlisthomework;
+
+/**
+ * Created by Rotem on 2/19/2020.
+ */
+
+public class Constants {
+
+    static final String LOGIN_PREF_NAME = "loginInformation";
+    static final String NOT_FOUND = "NOT FOUND";
+    static final String LOGIN_NAME_KEY = "name";
+    static final String LOGIN_PASSWORD_KEY = "password";
+}

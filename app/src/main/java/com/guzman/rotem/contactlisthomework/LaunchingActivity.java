5997a8b57d6f6822e24d5f99b2c39765e9d5fff3
RotemAAA <rotem.guzman@gmail.com>
2020-02-23 19:11:05 +0200

    homework 23/2/20, change colors for 4 second using handler

--- app/src/main/java/com/guzman/rotem/contactlisthomework/LaunchingActivity.java
+++ app/src/main/java/com/guzman/rotem/contactlisthomework/LaunchingActivity.java
@@ -1,23 +1,94 @@
 package com.guzman.rotem.contactlisthomework;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class LaunchingActivity extends AppCompatActivity {
 
+    Runnable runnable2;
+    int counter = 0;
+    int count = 0;
+    View view1, view2, view3, view4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launching);
 
-        //send();
-        if (chackLoggedIn()){
-            sendToContact();
-        }else {
-           sendToSignUp();
-        }
+        view1 = findViewById(R.id.view_1);
+        view2 = findViewById(R.id.view_2);
+        view3 = findViewById(R.id.view_3);
+        view4 = findViewById(R.id.view_4);
+
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @SuppressLint("WrongViewCast")
+            @Override
+            public void run() {
+                count++;
+                if (count <= 4) {
+//                    View view = findViewById(R.id.view_1);
+////                    ((TextView) findViewById(R.id.view_1)).setText("" + System.currentTimeMillis());
+//                view.setBackgroundColor(43234);
+                    updateViewColors();
+                    handler.postDelayed(this, 1000);
+                } else {
+                    if (chackLoggedIn()){
+                        sendToContact();
+                    }else {
+                        sendToSignUp();
+                    }
+                }
+            }
+        };
+
+        handler.postDelayed(runnable, 1000);
+
+
+//            Runnable runnable = new Runnable() {
+//                @Override
+//                public void run() {
+
+//                }
+//            };
+
+
+
+//        handler.postDelayed(runnable, 4000);
+       // handler.removeCallbacks(runnable);
+
+    }
+
+    private void updateViewColors(){
+        view1.setBackgroundColor(getResources().getColor(getRandomColor()));
+        view2.setBackgroundColor(getResources().getColor(getRandomColor()));
+        view3.setBackgroundColor(getResources().getColor(getRandomColor()));
+        view4.setBackgroundColor(getResources().getColor(getRandomColor()));
+    }
+    private int getRandomColor(){
+        ArrayList<Integer> color = new ArrayList<>();
+        color.add(R.color.colorAccent);
+        color.add(R.color.colorGreen);
+        color.add(R.color.colorPrimary);
+        color.add(R.color.colorPurple);
+        color.add(R.color.colorPrimaryDark);
+        color.add(R.color.colorRed);
+        color.add(R.color.colorWhite);
+        color.add(R.color.colorYellow);
+        //add colors
+        Random random = new Random();
+        int pointer = random.nextInt(color.size());
+
+        return color.get(pointer);
     }
 
     private boolean chackLoggedIn(){
@@ -47,7 +118,7 @@ public class LaunchingActivity extends AppCompatActivity {
 
     private void sendToContact(){
 
-        Intent intent = new Intent(LaunchingActivity.this, Constants.class);
+        Intent intent = new Intent(LaunchingActivity.this, ContactsActivity.class);
         startActivity(intent);
         finish();
     }
